668527617807c6843e4ab56b1bccd5b6bfeb596e
Michael W Chung <michaelwchungstudio@gmail.com>
2019-07-31 20:38:51 -0400

    getstarted

---
+++ javastart.java
@@ -0,0 +1,103 @@
+// Java
+// Programming language used for mobile applications (Android), desktop applications, web applications, web servers, games, database connections, etc.
+// It works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc.)
+// Open-source, free, secure, fast & powerful, large community support
+// Note: Java is case-sensitive
+
+// Quickstart
+// In Java, every application begins with a class name, and that class MUST MATCH the filename.
+// Every line of code must be inside a class.
+// A class should always start with an uppercase first letter.
+/*
+File: MyClass.java
+public class MyClass {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+*/
+
+// The 'main' method
+// The main() method is required and any code inside will be executed.
+/*
+public static void main(String[] args)
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
